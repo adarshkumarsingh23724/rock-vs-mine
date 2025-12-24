d8cb3115c93c59486b1fef3048d573a85d0e0949
adarshkumarsingh23724 <adarshkumar23724@gmail.com>
2025-12-24 16:12:32 +0530

    rock vs mine with dockerised form

--- myapp.py
+++ myapp.py
@@ -1,45 +1,34 @@
+from flask import Flask, request, render_template
 import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-import streamlit as st
-sonar_df=pd.read_csv('Copy of sonar data.csv',header=None)
-sonar_df.head()
-sonar_df.groupby(60).mean()
-sonar_df.describe()
-X=sonar_df.drop(columns=60,axis=1)
-y=sonar_df[60]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-model = LogisticRegression()     
-model.fit(X_train, y_train)    
-train_pred = model.predict(X_train)
-print(accuracy_score(train_pred, y_train))  
-train_pred = model.predict(X_test)
-print(accuracy_score(train_pred, y_test))  
+import pickle
 
-##we app---------
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-color: black;
-        color: white;
-    }
-    h1, h2, h3, h4, h5, h6, p, label, div {
-        color: white !important;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-st.title("Sonar Rock vs Mine Production")
-input_data=st.text_input("Enter here")
-if st.button('predict'):
-    input_data_np_array = np.asarray(input_data.split(','),dtype=float)
-    reshaped_input_data=input_data_np_array.reshape(1,-1)
-    prediction=model.predict(reshaped_input_data)
-    if prediction[0] == 'R':
-        st.write('this is a rock')
-    else:
-        st.write('this is a mine')
+# Load trained model
+with open("sonar_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        # Get input from form
+        input_data = request.form["features"]
+        input_data_np = np.asarray(input_data.split(","), dtype=float).reshape(1, -1)
+
+        prediction = model.predict(input_data_np)
+
+        if prediction[0] == "R":
+            result = "🪨 This object is a Rock"
+        else:
+            result = "💣 This object is a Mine"
+
+        return render_template("index.html", prediction_text=result)
+    except Exception as e:
+        return render_template("index.html", prediction_text=f"Error: {e}")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
